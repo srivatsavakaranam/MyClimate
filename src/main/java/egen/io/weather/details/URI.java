e3916049c9b561b6f08b2f290a5f63390c0a508a
srivatsavakaranam <sri_knm@yahoo.com>
2017-03-04 12:26:10 -0500

    Update URI.java

--- src/main/java/egen/io/weather/details/URI.java
+++ src/main/java/egen/io/weather/details/URI.java
@@ -11,8 +11,4 @@ public static final String WEATHER="{weather}";
 	public static final String AVG="{city}/average/{duration}/{property}";
 }
 
-//http://localhost:8080/myweather/weather---> FEED DATA
-//http://localhost:8080/myweather/weather/city/ ----> for total number of cities
-//http://localhost:8080/myweather/weather/city/property---> FOR PARTICULAR FILED
-//http://localhost:8080/myweather/weather/city/average/hourly/property---> FOR hourly avg
-//http://localhost:8080/myweather/weather/city/average/daily/property---> for daily avg
+
